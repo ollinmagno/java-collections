2c4f52ad2a6f26338dabf2624fbdd053b59a0498
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-10 19:41:24 -0300

    aprendendo collections

---
+++ src/collections/ExemploList.java
@@ -0,0 +1,39 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ExemploList {
+
+	public static void main(String[] args) {
+	
+		List<String> nomes = new ArrayList<>();
+		List<Integer> numeros = new ArrayList<>();
+		
+		numeros.add(5);
+		numeros.add(2);
+		numeros.add(50);
+		numeros.add(15);
+		numeros.add(30);
+		
+		Collections.sort(numeros);
+		System.out.println(numeros);
+		numeros.set(2, 1500);
+		System.out.println(numeros);
+		
+		nomes.add("Magno");
+		nomes.add("Juliana");
+		nomes.add("Maria");
+		nomes.add("Amanda");
+		nomes.add("Anderson");
+		nomes.add("Zelda");
+		
+		
+		Collections.sort(nomes);
+		System.out.println(nomes);
+		nomes.set(2, "Overwrite");
+		System.out.println(nomes);
+	}
+
+}
