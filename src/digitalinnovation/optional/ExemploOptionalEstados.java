82290302e8ba91334cd79f8571de94b0e3715134
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-26 21:11:58 -0300

    java.util.Optional

---
+++ src/digitalinnovation/optional/ExemploOptionalEstados.java
@@ -0,0 +1,13 @@
+package digitalinnovation.optional;
+
+import java.util.Optional;
+
+public class ExemploOptionalEstados {
+
+	public static void main(String[] args) {
+		Optional<String> optionalString = Optional.of("Valor presente");
+		System.out.println("Valor opcional que está presente");
+		
+	}
+
+}
